2d301909f5766247e38422818648a74f418428d2
AlehZahorski <git3371m@gmail.com>
2020-04-08 11:27:40 +0200

    allright homework

---
+++ homeWork/main.js
@@ -0,0 +1,24 @@
+const clickButton = document.querySelector('.classButton');
+
+let oddNumber = 1;
+function funcName() {
+    let newLi = document.createElement('li'); 
+    if(oddNumber%2==1){
+        console.log(`your number ${oddNumber} is odd, congratulations!`);
+        newLi.textContent = oddNumber;
+        if(oddNumber%3==0){
+            console.log(`your number ${oddNumber} is odd, congratulations!`);
+            newLi.textContent = oddNumber;  
+            // newLi.style.fontSize="30px";
+            newLi.classList.add('liClass')
+        }
+        oddNumber+=2;
+    }
+    let newLiStyle = document.getElementById('ulId')
+    newLiStyle.appendChild(newLi);
+
+}
+
+
+
+clickButton.addEventListener('click', funcName);
